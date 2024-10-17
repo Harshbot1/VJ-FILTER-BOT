be88171e67b6129f147b213c3fbde26445c9fded
Harshbot1 <165463132+Harshbot1@users.noreply.github.com>
2024-10-17 19:32:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -228,9 +228,8 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
-
-<b""" 
+    CAPTION = """
+    📂<b><blockquote><b><a href="https://t.me/INFINITE_MOVIES_2024">{file_name}</a></b></blockquote>"""""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
